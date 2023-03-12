ff2c63309c7bf29b23fe07961c292283578d76d2
mogoll92 <mogoll92@gmail.com>
2023-03-12 14:22:09 +0200

    Ironfish script

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ source $HOME/.bashrc
 WORKSPACE=$HOME/ironfish
 mkdir $WORKSPACE && chmod 755 $WORKSPACE
 
-sudo tee <<EOF >/dev/null $WORKING_DIR/docker-compose.yaml
+sudo tee <<EOF >/dev/null $WORKSPACE/docker-compose.yaml
 version: "3.3"
 services:
  ironfish:
